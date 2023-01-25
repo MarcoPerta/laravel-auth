668aeb2fedbeea2f57db9ba01226258eb28408d4
MarcoPerta <perta.marco@gmail.com>
2023-01-25 23:15:13 +0100

    auth

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+//Qui dobbiamo creare un modo per gestire le rotte sotto autenticazione
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin') //localhost:8080/admin/posts
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('index');
+        
 });
+
+//Qui dobbiamo trovare un  modo per gestire tutte le rotte che non usano autenticazione
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*");
